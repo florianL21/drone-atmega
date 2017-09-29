fde712789973606b8e64be9417dc4f129d38de07
lorki97 <lorenz.markus97@gmail.com>
2017-09-29 19:32:41 +0200

    Init Commit

---
+++ Drohne/ESCControl.h
@@ -0,0 +1,23 @@
+/*
+ * ESCControl.h
+ *
+ * Created: 29.09.2017 19:14:02
+ *  Author: Markus Lorenz
+ */ 
+
+
+#ifndef ESCCONTROL_H_
+#define ESCCONTROL_H_
+
+
+/*
+ *
+ *	Configures the 4 ESC pins as output and
+ *	
+ *
+ */
+void esc_init();
+
+
+
+#endif /* ESCCONTROL_H_ */
---
+++ Drohne/main.c
@@ -0,0 +1,19 @@
+/*
+ * Drohne.c
+ *
+ * Created: 29.09.2017 19:08:07
+ * Author : Markus Lorenz
+ */ 
+
+#include <avr/io.h>
+#include "ESCControl.h"
+
+int main(void)
+{
+	es
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
