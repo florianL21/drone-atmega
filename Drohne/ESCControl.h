03a97feadf5ca6ffe73af93f3af495ecd7ad692c
lorki97 <lorenz.markus97@gmail.com>
2017-09-29 19:44:35 +0200

    Header file done

--- Drohne/ESCControl.h
+++ Drohne/ESCControl.h
@@ -10,14 +10,56 @@
 #define ESCCONTROL_H_
 
 
+/*	MOTOR LAYOUT
+
+|----|        |----|
+| M1 |        | M2 |
+|----|        |----|
+
+
+
+|----|        |----|
+| M3 |        | M4 |
+|----|        |----|
+
+ */
+
+
 /*
  *
  *	Configures the 4 ESC pins as output and
- *	
+ *	Timer 3 & 4
  *
  */
 void esc_init();
 
+/*
+ *	Sets the Timer OCR register for ESC M1
+ *	Implements value validation (min-max)
+ *
+ */
+void esc_set_m1(int16_t speed);
+
+/*
+ *	Sets the Timer OCR register for ESC M2
+ *	Implements value validation (min-max)
+ *
+ */
+void esc_set_m2(int16_t speed);
+
+/*
+ *	Sets the Timer OCR register for ESC M3
+ *	Implements value validation (min-max)
+ *
+ */
+void esc_set_m3(int16_t speed);
+
+/*
+ *	Sets the Timer OCR register for ESC M4
+ *	Implements value validation (min-max)
+ *
+ */
+void esc_set_m4(int16_t speed);
 
 
 #endif /* ESCCONTROL_H_ */
---
+++ Drohne/config.h
@@ -0,0 +1,38 @@
+/*
+ * config.h
+ *
+ * Created: 29.09.2017 19:37:39
+ *  Author: Markus Lorenz
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/*
+ *
+ *	ESC Configuration
+ *
+ */
+
+// M1 ESC Timer OCR Offset
+#define ESC_Offset_M1 50
+// M2 ESC Timer OCR Offset
+#define ESC_Offset_M2 295
+// M3 ESC Timer OCR Offset
+#define ESC_Offset_M3 298
+// M4 ESC Timer OCR Offset
+#define ESC_Offset_M4 294
+
+// M1 ESC Slope Compensation Factor
+#define ESC_SlopeComp_M1 0.38
+// M2 ESC Slope Compensation Factor
+#define ESC_SlopeComp_M2 0
+// M3 ESC Slope Compensation Factor
+#define ESC_SlopeComp_M3 0
+// M4 ESC Slope Compensation Factor
+#define ESC_SlopeComp_M4 0
+
+
+
+#endif /* CONFIG_H_ */
