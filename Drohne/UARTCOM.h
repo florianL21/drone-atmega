cd1c79d3c35b3d7d89dc97aa68ab9c9abf08578a
florianL21 <flo.laschober@gmail.com>
2017-10-02 15:28:54 +0200

    Added Function descriptions

--- Drohne/UARTCOM.h
+++ Drohne/UARTCOM.h
@@ -32,12 +32,33 @@ typedef struct transmissionData transmissionData;
 
 #define LENGTH(x)  (sizeof(x) / sizeof((x)[0]))
 
-void uart_recived_char(uint8_t recvChar);
+/*
+* Initializes everything needet for the UARTCOMM Module
+* the interrupts have to be enabled afterwards manually
+*/
 void UARTCOM_init(uint32_t BaudRate);
-bool UARTCOM_ready_to_send(transmissionData Data);
-void UARTCOM_transmit_block(transmissionData Data);
-transmissionData UARTCOM_get_data_block(uint8_t Type, const uint8_t Data[], uint8_t Length);
 
+
+/*
+* Checks if there is enought space in the send buffer to send the required data
+* Suggested implementation (LENGTH only works if the dataArray is NOT passed by a pointer!): 
+* ...
+* uint8_t dataArray[] = { ... };
+* if( UARTCOMM_ready_to_send(LENGTH(dataArray)) )
+* {
+* 	UARTCOM_transmit_block( ... );
+* }
+*/
+bool UARTCOM_ready_to_send(transmissionData Data);
+
+/*
+* Sends data over the UARTCOMM Module after a safety check.
+* Returns the data structure if sucessful, returns NULL if unsucsessful
+* Suggested implementation (LENGTH only works if the dataArray is NOT passed by a pointer!): 
+* uint8_t dataArray[] = { ... };
+* UARTCOM_transmit_block(0-256, dataArray, LENGTH(dataArray));
+*/
+transmissionData UARTCOM_transmit_block(uint8_t Type, const uint8_t Data[], uint8_t Length);
 
 
 #endif /* UARTCOM_H_ */
